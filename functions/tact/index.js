952f88d23e4b645e831a35c3ab8a6d97db3f4ff7
Ferronn <ferronn@ferronn.dev>
2022-11-22 20:22:40 +0000

    add md5 dependency

--- functions/tact/index.js
+++ functions/tact/index.js
@@ -1,6 +1,9 @@
-const functions = require('@google-cloud/functions-framework');
 const axios = require('axios');
+const functions = require('@google-cloud/functions-framework');
+const md5 = require('md5');
+
 functions.http('function', async (req, res) => {
-  console.log(await axios.get('http://us.patch.battle.net:1119/wow/versions'));
-  res.send('OK');
+  let result = await axios.get('http://us.patch.battle.net:1119/wow/versions');
+  let hash = md5(result.data);
+  res.send(hash);
 });
