af9ec74e5060a023449847105dce6f21681b83a2
Ferronn <ferronn@ferronn.dev>
2022-11-22 20:16:03 +0000

    jshint pre-commit

--- functions/tact/index.js
+++ functions/tact/index.js
@@ -1,6 +1,6 @@
 const functions = require('@google-cloud/functions-framework');
 const axios = require('axios');
 functions.http('function', async (req, res) => {
-  console.log(await axios.get('http://us.patch.battle.net:1119/wow/versions'))
-  res.send('OK')
+  console.log(await axios.get('http://us.patch.battle.net:1119/wow/versions'));
+  res.send('OK');
 });
