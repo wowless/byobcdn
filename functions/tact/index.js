6866e5781fea1e7421e30e6458e7e802fdecd3c3
Ferronn <ferronn@ferronn.dev>
2022-11-22 20:38:48 +0000

    write content to cloud storage

--- functions/tact/index.js
+++ functions/tact/index.js
@@ -1,9 +1,13 @@
 const axios = require('axios');
 const functions = require('@google-cloud/functions-framework');
 const md5 = require('md5');
+const storage = require('@google-cloud/storage');
+
+const bucket = new storage.Storage().bucket('wowless.dev');
 
 functions.http('function', async (req, res) => {
   let result = await axios.get('http://us.patch.battle.net:1119/wow/versions');
   let hash = md5(result.data);
+  await bucket.file('byobcdn/content/' + hash).save(result.data);
   res.send(hash);
 });
