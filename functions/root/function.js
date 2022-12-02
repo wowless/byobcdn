9680fe64a03b462ae9e6a9992dc3e43c58a8bc50
Ferronn <ferronn@ferronn.dev>
2022-12-02 15:08:01 +0000

    introduce byobcdn-root

---
+++ functions/root/function.js
@@ -0,0 +1,55 @@
+const functions = require('@google-cloud/functions-framework');
+const {PubSub} = require('@google-cloud/pubsub');
+const pubsub = new PubSub();
+
+const products = [
+  "wow",
+  "wowt",
+  "wow_beta",
+  "wow_classic",
+  "wow_classic_era",
+  "wow_classic_era_ptr",
+  "wow_classic_ptr",
+];
+
+const regions = [
+  "cn",
+  "eu",
+  "kr",
+  "us",
+];
+
+const endpoints = [
+  "cdns",
+  "versions",
+];
+
+const messages = [];
+for (const region of regions) {
+  for (const product of products) {
+    for (const endpoint of endpoints) {
+      messages.push({
+        json: {
+          endpoint: endpoint,
+          product: product,
+          region: region,
+        },
+      });
+    }
+  }
+}
+
+const pub = pubsub.topic('byobcdn-root', {
+  batching: {
+    maxMessages: messages.length,
+  }
+});
+
+async function publish(m) {
+  await pub.publishMessage(m);
+}
+
+functions.http('function', async (_, res) => {
+  await Promise.all(messages.map(publish));
+  res.send('OK\n');
+});
--- main.tf
+++ main.tf
@@ -112,3 +112,60 @@ resource "google_cloud_scheduler_job" "byobcdn-tact-versions-crons" {
     retry_count          = 0
   }
 }
+
+resource "google_service_account" "byobcdn-root-runner" {
+  account_id   = "byobcdn-root-runner"
+  display_name = "byobcdn-root-runner"
+}
+
+resource "google_pubsub_topic" "byobcdn-root" {
+  name = "byobcdn-root"
+}
+
+resource "google_cloudfunctions_function" "byobcdn-root" {
+  name                  = "byobcdn-root"
+  runtime               = "nodejs18"
+  entry_point           = "function"
+  available_memory_mb   = 128
+  trigger_http          = true
+  service_account_email = google_service_account.byobcdn-root-runner.email
+  lifecycle {
+    ignore_changes = [
+      labels["deployment-tool"],
+    ]
+  }
+  timeouts {}
+}
+
+resource "google_service_account" "byobcdn-root-invoker" {
+  account_id   = "byobcdn-root-invoker"
+  display_name = "byobcdn-root-invoker"
+}
+
+data "google_iam_policy" "byobcdn-root-invoker" {
+  binding {
+    members = [
+      "serviceAccount:${google_service_account.byobcdn-root-runner.email}",
+    ]
+    role = "roles/iam.serviceAccountUser"
+  }
+}
+
+resource "google_service_account_iam_policy" "byobcdn-root-invoker" {
+  service_account_id = google_service_account.byobcdn-root-invoker.name
+  policy_data        = data.google_iam_policy.byobcdn-root-invoker.policy_data
+}
+
+data "google_iam_policy" "byobcdn-root" {
+  binding {
+    members = [
+      "serviceAccount:${google_service_account.byobcdn-root-invoker.email}",
+    ]
+    role = "roles/cloudfunctions.invoker"
+  }
+}
+
+resource "google_cloudfunctions_function_iam_policy" "byobcdn-root" {
+  cloud_function = google_cloudfunctions_function.byobcdn-root.name
+  policy_data    = data.google_iam_policy.byobcdn-root.policy_data
+}
