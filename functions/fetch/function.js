64b42ba5e3594624d464fafec76a699f7995523b
Ferronn <ferronn@ferronn.dev>
2022-12-05 14:48:05 +0000

    fetch: if name supplied, check exists first

--- functions/fetch/function.js
+++ functions/fetch/function.js
@@ -7,23 +7,36 @@ const bucket = new storage.Storage().bucket(process.env.BYOBCDN_BUCKET);
 
 functions.http('function', async (req, res) => {
   const {url, path, name} = req.body;
-  const result = await axios.get(url);
-  const file = bucket.file(`byobcdn/${path}/${name || md5(result.data)}`);
-  var written = false;
-  try {
-    await file.save(result.data, {
-      preconditionOpts: {
-        ifGenerationMatch: 0,
-      }
-    });
-    written = true;
-  } catch (error) {
-    if (error.code != 412) {
-      throw error;
+  if (name) {
+    const file = bucket.file(`byobcdn/${path}/${name}`);
+    const exists = await file.exists();
+    if (!exists) {
+      const result = await axios.get(url);
+      await file.save(result.data);
     }
+    res.json({
+      uri: file.cloudStorageURI,
+      written: !exists,
+    });
+  } else {
+    const result = await axios.get(url);
+    const file = bucket.file(`byobcdn/${path}/${md5(result.data)}`);
+    var written = false;
+    try {
+      await file.save(result.data, {
+        preconditionOpts: {
+          ifGenerationMatch: 0,
+        }
+      });
+      written = true;
+    } catch (error) {
+      if (error.code != 412) {
+        throw error;
+      }
+    }
+    res.json({
+      uri: file.cloudStorageURI,
+      written: written,
+    });
   }
-  res.json({
-    uri: file.cloudStorageURI,
-    written: written,
-  });
 });
