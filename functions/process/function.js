a34a28e82d9aa623e0717abd7668447ba26de3cf
Ferronn <ferronn@ferronn.dev>
2022-12-06 22:49:13 +0000

    rewrite process

--- functions/process/function.js
+++ functions/process/function.js
@@ -33,40 +33,64 @@ function mkurl(kind, hash, suffix) {
   return `http://level3.blizzard.com/tpr/wow/${kind}/${ab}/${cd}/${hash}${suffix || ''}`;
 }
 
+const handlers = [
+  {
+    name: 'tact product version',
+    pattern: /^byobcdn\/tact\/[^/]+\/[^/]+\/versions\//,
+    tasks: function*(content) {
+      const config = tactpipe(content.toString()).data[0];
+      yield {
+        path: 'tact/build',
+        url: mkurl('config', config.BuildConfig),
+      };
+      yield {
+        path: 'tact/cdn',
+        url: mkurl('config', config.CDNConfig),
+      };
+    },
+  },
+  {
+    name: 'tact cdn config',
+    pattern: /^byobcdn\/tact\/cdn\/[0-9a-f]+$/,
+    tasks: function*(content) {
+      for (const archive of tactcfg(content.toString()).data.archives) {
+        yield {
+          name: archive,
+          path: 'index',
+          url: mkurl('data', archive, '.index'),
+        };
+        yield {
+          name: archive,
+          path: 'archive',
+          url: mkurl('data', archive),
+        };
+      }
+    },
+  },
+];
+
 functions.http('function', async (req, res) => {
   const {bucket, name} = req.body;
   const file = storage.bucket(bucket).file(name);
-  if (file.name.match(/^byobcdn\/tact\/[^/]+\/[^/]+\/versions\//)) {
-    const content = await file.download();
-    const config = tactpipe(content.toString()).data[0];
-    await mktask({
-      path: 'tact/build',
-      url: mkurl('config', config.BuildConfig),
-    });
-    await mktask({
-      path: 'tact/cdn',
-      url: mkurl('config', config.CDNConfig),
-    });
-  } else if (file.name.match(/^byobcdn\/tact\/cdn\/[0-9a-f]+$/)) {
-    const content = await file.download();
-    const tasks = [];
-    for (const archive of tactcfg(content.toString()).data.archives) {
-      tasks.push(mktask({
-        name: archive,
-        path: 'index',
-        url: mkurl('data', archive, '.index'),
-      }));
-      tasks.push(mktask({
-        name: archive,
-        path: 'archive',
-        url: mkurl('data', archive),
-      }));
-      if (tasks.length >= 20) {
-        await Promise.all(tasks);
-        tasks.length = 0;
+  for (const handler of handlers) {
+    if (file.name.match(handler.pattern)) {
+      var total = 0;
+      const tasks = [];
+      for (const task of handler.tasks(await file.download())) {
+        total++;
+        tasks.push(mktask(task));
+        if (tasks.length >= 20) {
+          await Promise.all(tasks);
+          tasks.length = 0;
+        }
       }
+      await Promise.all(tasks);
+      res.json({
+        name: handler.name,
+        count: total,
+      });
+      return;
     }
-    await Promise.all(tasks);
   }
   res.json({});
 });
