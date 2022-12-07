20524f917b0d6812073bbb3040cdd11ae76ea839
Ferronn <ferronn@ferronn.dev>
2022-12-07 19:59:32 +0000

    fetch: response type arraybuffer to avoid corruption

--- functions/fetch/function.js
+++ functions/fetch/function.js
@@ -5,25 +5,34 @@ const md5 = require('md5');
 
 const bucket = new storage.Storage().bucket(process.env.BYOBCDN_BUCKET);
 
+async function fetch(url) {
+  const result = await axios({
+    method: 'get',
+    responseType: 'arraybuffer',
+    url: url,
+  });
+  return result.data;
+}
+
 functions.http('function', async (req, res) => {
   const {url, path, name} = req.body;
   if (name) {
     const file = bucket.file(`byobcdn/${path}/${name}`);
     const [exists] = await file.exists();
     if (!exists) {
-      const result = await axios.get(url);
-      await file.save(result.data);
+      const result = await fetch(url);
+      await file.save(result);
     }
     res.json({
       uri: file.cloudStorageURI,
       written: !exists,
     });
   } else {
-    const result = await axios.get(url);
-    const file = bucket.file(`byobcdn/${path}/${md5(result.data)}`);
+    const result = await fetch(url);
+    const file = bucket.file(`byobcdn/${path}/${md5(result)}`);
     var written = false;
     try {
-      await file.save(result.data, {
+      await file.save(result, {
         preconditionOpts: {
           ifGenerationMatch: 0,
         }
