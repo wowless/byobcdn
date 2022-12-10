9c0bb40409ec4c328b8d9c10655db5a1703c06ab
Ferronn <ferronn@ferronn.dev>
2022-12-10 04:41:45 +0000

    tactpoints, datastore

--- functions/tact/function.js
+++ functions/tact/function.js
@@ -2,14 +2,17 @@ const axios = require('axios');
 const functions = require('@google-cloud/functions-framework');
 const md5 = require('md5');
 const storage = require('@google-cloud/storage');
+const { Datastore } = require('@google-cloud/datastore');
 
 const bucket = new storage.Storage().bucket(process.env.BYOBCDN_BUCKET);
+const datastore = new Datastore();
 
 functions.cloudEvent('function', async (cloudEvent) => {
   const q = JSON.parse(Buffer.from(cloudEvent.data.message.data, 'base64').toString());
+  const timestamp = new Date();
   const result = await axios.get(`http://${q.region}.patch.battle.net:1119/${q.product}/${q.endpoint}`);
   const hash = md5(result.data);
-  const path = `byobcdn/tact/${q.region}/${q.product}/${q.endpoint}/${hash}`;
+  const path = `byobcdn/tactpoints/${q.endpoint}/${hash}`;
   try {
     await bucket.file(path).save(result.data, {
       preconditionOpts: {
@@ -21,4 +24,14 @@ functions.cloudEvent('function', async (cloudEvent) => {
       throw error;
     }
   }
+  await datastore.save({
+    key: datastore.key(['TactPoint']),
+    data: {
+      endpoint: q.endpoint,
+      hash: hash,
+      product: q.product,
+      region: q.region,
+      timestamp: timestamp,
+    }
+  });
 });
