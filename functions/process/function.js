b23042af0e04ec84d7df19a993b884655cd47809
Ferronn <ferronn@ferronn.dev>
2022-12-12 20:03:23 +0000

    replace tactpipe and tactcfg with tactless

--- functions/process/function.js
+++ functions/process/function.js
@@ -2,11 +2,7 @@ const { Datastore } = require('@google-cloud/datastore');
 const functions = require('@google-cloud/functions-framework');
 const { Storage } = require('@google-cloud/storage');
 const { CloudTasksClient } = require('@google-cloud/tasks');
-const { parse: tactcfg } = require('tactcfg');
-const { parse: tactpipe } = require('tactpipe');
-const { Parser } = require('binary-parser');
-const assert = require('node:assert');
-const md5 = require('md5');
+const tactless = require('tactless');
 
 const datastore = new Datastore();
 const storage = new Storage();
@@ -38,69 +34,6 @@ function mkurl(kind, hash, suffix) {
   return `http://level3.blizzard.com/tpr/wow/${kind}/${ab}/${cd}/${hash}${suffix || ''}`;
 }
 
-const archiveIndexFooterParser = new Parser()
-  .buffer("tocChecksum", { length: 8 })
-  .uint8("version", { assert: 1 })
-  .uint8("f3", { assert: 0 })
-  .uint8("f4", { assert: 0 })
-  .uint8("blockSize", { assert: 4 })
-  .uint8("offsetBytes", { assert: 4 })
-  .uint8("sizeBytes", { assert: 4 })
-  .uint8("keySize", { assert: 16 })
-  .uint8("checksumSize", { assert: 8 })
-  .uint32le("numElements")
-  .buffer("checksum", { length: 8 });
-
-function sub(buf, offset, size) {
-  return buf.subarray(offset, offset + size);
-}
-
-function parseArchiveIndex(content, name) {
-  const footerOffset = content.length - 28;
-  assert(footerOffset >= 0, "footer length");
-  const footerBytes = content.subarray(footerOffset);
-  assert.strictEqual(md5(footerBytes), name, "footer checksum");
-  const blockSize = 4096;
-  const bytesPerBlock = blockSize + 24;
-  assert.strictEqual(footerOffset % bytesPerBlock, 0, "content size");
-  const footer = archiveIndexFooterParser.parse(footerBytes);
-  const numBlocks = Math.trunc(footerOffset / bytesPerBlock);
-  const blockHashesOffset = footerOffset - numBlocks * 8;
-  const lastEntriesOffset = blockHashesOffset - numBlocks * 16;
-  assert.strictEqual(
-    md5(content.subarray(lastEntriesOffset, footerOffset)).substring(0, 16),
-    footer.tocChecksum.toString('hex'),
-    "toc checksum");
-  assert.strictEqual(
-    md5(Buffer.concat([
-      content.subarray(footerOffset + 8, footerOffset + 20),
-      Buffer.from('\0'.repeat(8)),
-    ])).substring(0, 16),
-    footer.checksum.toString('hex'),
-    "internal footer checksum");
-  const result = [];
-  for (let i = 0; i < numBlocks; ++i) {
-    const block = sub(content, blockSize * i, blockSize);
-    const lastEntry = sub(content, lastEntriesOffset + i * 16, 16).toString('hex');
-    const blockHash = sub(content, blockHashesOffset + i * 8, 8).toString('hex');
-    assert.strictEqual(md5(block).substring(0, 16), blockHash, "block hash");
-    let found = false;
-    for (let p = 0; p <= blockSize - 24 && !found; p += 24) {
-      const ekey = block.subarray(p, p + 16).toString('hex');
-      const size = block.readUInt32BE(p + 16);
-      const offset = block.readUInt32BE(p + 20);
-      result.push({
-        ekey: ekey,
-        offset: offset,
-        size: size,
-      });
-      found = ekey == lastEntry;
-    }
-    assert(found, "last key found");
-  }
-  return result;
-}
-
 async function batchit(fn, size, gen) {
   const batch = [];
   let numElements = 0;
@@ -123,7 +56,7 @@ const handlers = [
     name: 'tact build config',
     pattern: /^byobcdn\/tact\/build\//,
     process: async (content) => {
-      const hash = tactcfg(content.toString()).data.encoding[1];
+      const hash = tactless.config(content.toString()).data.encoding[1];
       await mkfetchtask({
         name: hash,
         path: 'encoding',
@@ -135,7 +68,7 @@ const handlers = [
     name: 'tact product version',
     pattern: /^byobcdn\/tactpoints\/versions\//,
     process: async (content) => {
-      const config = tactpipe(content.toString()).data[0];
+      const config = tactless.pipe(content.toString()).data[0];
       await mkfetchtask({
         path: 'tact/build',
         url: mkurl('config', config.BuildConfig),
@@ -151,7 +84,7 @@ const handlers = [
     pattern: /^byobcdn\/tact\/cdn\/[0-9a-f]+$/,
     process: async (content) => {
       await batchit(x => Promise.all(x), 20, function* () {
-        for (const archive of tactcfg(content.toString()).data.archives) {
+        for (const archive of tactless.config(content.toString()).data.archives) {
           yield mkfetchtask({
             name: archive,
             path: 'index',
@@ -183,7 +116,7 @@ const handlers = [
       });
       console.log('saving new entries');
       await batchit(x => datastore.save(x), 500, function* () {
-        for (const entry of parseArchiveIndex(content, archiveName)) {
+        for (const entry of tactless.index(content, archiveName)) {
           yield {
             key: datastore.key(['ArchiveEntry']),
             data: {
--- functions/www/function.js
+++ functions/www/function.js
@@ -1,7 +1,6 @@
 const datastore = new (require('@google-cloud/datastore').Datastore)();
 const bucket = new (require('@google-cloud/storage').Storage)().bucket('byobcdn.wowless.dev');
-const { parse: tactcfg } = require('tactcfg');
-const { parse: tactpipe } = require('tactpipe');
+const tactless = require('tactless');
 
 const app = require('express')();
 app.set('view engine', 'pug');
@@ -48,7 +47,7 @@ app.get('/build/:id', async (req, res) => {
   const file = bucket.file(`byobcdn/tact/build/${req.params.id}`);
   const [content] = await file.download();
   res.render('buildconfig', {
-    config: tactcfg(content.toString()).data,
+    config: tactless.config(content.toString()).data,
     id: req.params.id,
   });
 });
@@ -57,7 +56,7 @@ app.get('/cdn/:id', async (req, res) => {
   const file = bucket.file(`byobcdn/tact/cdn/${req.params.id}`);
   const [content] = await file.download();
   res.render('cdnconfig', {
-    config: tactcfg(content.toString()).data,
+    config: tactless.config(content.toString()).data,
     id: req.params.id,
   });
 });
@@ -78,7 +77,7 @@ app.get('/versions/:id', async (req, res) => {
   const file = bucket.file(`byobcdn/tactpoints/versions/${req.params.id}`);
   const [content] = await file.download();
   res.render('versions', {
-    config: tactpipe(content.toString()).data[0],
+    config: tactless.pipe(content.toString()).data[0],
     id: req.params.id,
   });
 });
